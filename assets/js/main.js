5e4fdc4049da1a928266a775a70ded7bfec443fc
NallyNik <nallynik@gmail.com>
2023-04-12 15:03:13 -0300

    js

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+async function fetchData() {
+    const url = 'https://raw.githubusercontent.com/NallyNik/fm-expenses-chart-component/master/assets/data.json';
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+function updateSpan (data) {
+    const spans = document.querySelectorAll('.amount');
+    for (let i = 0; i < spans.length; i++) {
+        spans[i].innerHTML = `$${data[i].amount}`;
+    }
+}
+
+function updateItem (data) {
+    const items = document.querySelectorAll('.item');
+    for (let i = 0; i < items.length; i++) {
+        items[i].style.height = `${data[i].amount*2}px`;
+    }
+}
+
+function updateDays (data) {
+    const days = document.querySelectorAll('.day');
+    for (let i = 0; i < days.length; i++) {
+        days[i].innerHTML = data[i].day;
+    }
+}
+
+(async () => {
+    const data = await fetchData();
+    updateSpan(data);
+    updateItem(data);
+    updateDays(data);
+})();
+    
+
+    
